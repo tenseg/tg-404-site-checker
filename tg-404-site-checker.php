33ebfe06cdac88973505c475bfccd074d046b3b1
Alexander Celeste <alex@tenseg.net>
2021-08-05 13:09:14 -0500

    frontmatter uri fixes

--- tg-404-site-checker.php
+++ tg-404-site-checker.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: TG 404 Site Checker
-Plugin URI: https://tenseg.net
+Plugin URI: https://www.tenseg.net
 Description: Check another site for the requested path and redirect there during 404.
 Version: 1.0
 Author: Tenseg LLC
-Author URI: https://tenseg.net
+Author URI: https://www.tenseg.net
 License: MIT License
 Text Domain: tg-404-site-checker
  */
